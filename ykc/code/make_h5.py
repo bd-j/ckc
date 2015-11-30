a6e0d26b9ece18120ef3cf9ffad798aff02144af
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2015-11-29 20:19:27 -0500

    Speed up FFT smoothing, script to make h5 file
    Many changes to clean up the FFT smoothing, including especially the trimming
    and interpolation to get spectra with length that is a power of two, for faster
    FFT, and cleaner tests of different smoothing schemes.  Also added a script to
    make an h5 file from the convolved results.

---
+++ ykc/code/make_h5.py
@@ -0,0 +1,55 @@
+import os, sys, time
+from itertools import product
+import numpy as np
+import h5py
+import ykc
+
+full_params = {'t': np.arange(4000, 5600, 100),
+               'g': [1.0, 1.5, 2.0],
+               'feh': np.arange(-4, 1.0, 0.5),
+               'afe': [-0.2, 0.0, 0.2, 0.4, 0.6, 0.8],
+               'nfe': [0, 0.3], 'cfe': [0, 0.3],
+               'vturb':[0.5, 3.5]}
+
+def get_spectrum(param):
+    pars = dict(zip(ykc.param_order, param))
+    w, s = ykc.convolve_lam_onestep(fwhm=1.0, wlo=4e3, whi=1.1e4, **pars)
+    return w, s
+
+
+def param_map(ps):
+    """Logify Teff
+    """
+    ps[0] = np.log10(ps[0])
+    return tuple(ps)
+    
+
+pname_map = {'t':'logt', 'g':'logg', 'feh':'feh', 'afe':'afe', 'nfe':'nfe', 'cfe':'cfe', 'vturb':'vturb'}
+pnames = [pname_map[p] for p in ykc.param_order]
+
+
+if __name__ == "__main__":
+    h5name = 'test.h5'
+    paramlists = {'t': [5000],
+                  'g': [1.0],
+                  'feh': [0.0],
+                  'afe': [-0.2, 0.0, 0.2, 0.4, 0.6, 0.8],
+                  'nfe': [0, 0.3], 'cfe': [0, 0.3],
+                  'vturb':[0.5, 3.5]}
+
+    params = list(product(*[paramlists[p] for p in ykc.param_order]))
+    nspec, npar = len(params), len(params[0])
+    dt = np.dtype(zip(pnames, npar * [np.float]))
+    pars = np.empty(nspec, dtype=dt)
+
+    with h5py.File(h5name, 'w') as f:
+        w, s = get_spectrum(params[0])
+        nwave = len(w)
+        spec = f.create_dataset('spectra', (nspec, nwave))
+        pset = f.create_dataset('parameters', data=pars)
+        wave = f.create_dataset('wavelengths', data=w)
+        for i, p in enumerate(params):
+            w, s = get_spectrum(p)
+            spec[i,:] = s
+            pset[i] = tuple(param_map(list(p)))
+
--- ykc/code/smooth_fft.py
+++ ykc/code/smooth_fft.py
@@ -6,62 +6,101 @@ ckms = 2.998e5
 sigma_to_fwhm = 2.355
 
 
+def mask_wave(wavelength, R=20000, wlo=0, whi=np.inf, outwave=None,
+              nsigma_pad=20.0, linear=False, **extras):
+    """restrict wavelength range (for speed) but include some padding"""
+    # Base wavelength limits
+    if outwave is not None:
+        wlim = np.array([outwave.min(), outwave.max()])
+    else:
+        wlim = np.array([wlo, whi])
+    # Pad by nsigma * sigma_wave
+    if linear:
+        wlim += nsigma_pad * R * np.array([-1, 1])
+    else:
+        wlim *= (1 + nsigma_pad / R * np.array([-1, 1]))
+    mask = (wavelength > wlim[0]) & (wavelength < wlim[1])
+    return mask
+
+
+def resample_wave(wavelength, spectrum, linear=False):
+    """Resample spectrum, so that the number of elements is the next highest
+    power of two.  Assumes the input spectrum is constant velocity resolution
+    unless ``linear`` is True, in which case it is assumed that the spectrum has
+    constant wavelength resolution (e.g. 1AA)
+    """
+    wmin, wmax = wavelength.min(), wavelength.max()
+    nw = len(wavelength)
+    nnew = 2.0**(np.ceil(np.log2(nw)))
+    if linear:
+        Rgrid = np.diff(wavelength) # in same units as ``wavelength`` 
+        w = np.linspace(wmin, wmax, nnew)
+    else:
+        Rgrid = np.diff(np.log(wavelength))  # actually 1/R
+        lnlam = np.linspace(np.log(wmin), np.log(wmax), nnew)
+        w = np.exp(lnlam)
+    # Make sure the resolution really is nearly constant
+    assert Rgrid.max() / Rgrid.min() < 1.05
+    s = np.interp(w, wavelength, spectrum)
+    return w, s
+
+
 def smooth_vel_fft(wavelength, spectrum, outwave=None,
-                   wlo=0, whi=np.inf, nsigma_pad=10,
-                   Rout=20000, Rin=3e5):
+                   Rout=20000, Rin=3e5, **extras):
     """wave, spec: A log-lam gridded spectrum
     Rin: wave / sigma_wave
     Rout: wave / sigma_wave
     """
-
+    
     # restrict wavelength range (for speed)
-    # should also make power of two
-    wlim = np.array([wlo, whi]) * (1 + nsigma_pad / Rout * np.array([-1, 1]))
-    g = (wavelength > wlim[0]) & (wavelength < wlim[1])
-    wave = wavelength[g]
-    spec = spectrum[g]
+    # should also make power of 2
+    mask = mask_wave(wavelength, outwave=outwave, R=Rout, **extras)    
+    wave, spec = resample_wave(wavelength[mask], spectrum[mask])
 
     # The kernel width for the convolution.
     sigma_out, sigma_in = ckms / Rout, ckms / Rin
     sigma = np.sqrt(sigma_out**2 - sigma_in**2)
+    if sigma < 0:
+        raise(ValueError)
     
-    # get grid resolution and make sure its nearly constant.
-    Rgrid = ((wave[:-1] + wave[1:])/2.) / np.diff(wave)
-    assert Rgrid.max() / Rgrid.min() < 1.05
-    dv = ckms / np.median(Rgrid)
+    # get grid resolution (*not* the resolution of the input spectrum) and make
+    # sure it's nearly constant.  Should be by design (see resample_wave above)
+    invRgrid =  np.diff(np.log(wave))
+    assert invRgrid.max() / invRgrid.min() < 1.05
+    dv = ckms * np.median(invRgrid)
 
     # Do the convolution
     spec_conv = smooth_fft(dv, spec, sigma)
     # interpolate onto output grid
+    print(len(wave), len(spec_conv), len(spec))
     if outwave is not None:
-        spec_conv = np.interp(outwave, wave[:-1], spec_conv)
+        spec_conv = np.interp(outwave, wave, spec_conv)
 
     return spec_conv
 
 
 def smooth_wave_fft(wavelength, spectrum, outwave=None,
-                    wlo=0, whi=np.inf, nsigma_pad=10,
-                    sigma_out=1.0, sigma_in=0.0):
-    """Wave, spec: a spectrum evenly gridded in linear-lam.
+                    sigma_out=1.0, sigma_in=0.0, **extras):
+    """Wave, spec: a linear-lam gridded spectrum.
     """
     # restrict wavelength range (for speed)
-    # should also make nearest power of two
-    wlim = np.array([wlo, whi]) + nsigma_pad * sigma_out * np.array([-1, 1])
-    g = (wavelength > wlim[0]) & (wavelength < wlim[1])
-    wave = wavelength[g]
-    spec = spectrum[g]
+    # should also make nearest power of 2
+    mask = mask_wave(wavelength, outwave=outwave, R=sigma_out, linear=True, **extras)
+    wave, spec = resample_wave(wavelength[mask], spectrum[mask], linear=True)
     
     # The kernel width for the convolution.
     sigma = np.sqrt(sigma_out**2 - sigma_in**2)
+    if sigma < 0:
+        raise(ValueError)
     
-    # get grid resolution and make sure its nearly constant.
+    # get grid resolution (*not* the resolution of the input spectrum) and make
+    # sure it's nearly constant.  Should be by design (see resample_wave above)
     Rgrid = np.diff(wave)
     assert Rgrid.max() / Rgrid.min() < 1.05
     dw = np.median(Rgrid)
     
     # Do the convolution
     spec_conv = smooth_fft(dw, spec, sigma)
-    print(len(spec), len(wave), len(spec_conv))
     # interpolate onto output grid
     if outwave is not None:
         spec_conv = np.interp(outwave, wave, spec_conv)
