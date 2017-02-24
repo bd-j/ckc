ab0c9cf1d8bb09d4c015b7a13927a3f0fe6fcf59
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2017-02-24 11:56:48 -0500

    cleanup and generalize make_full_h5

--- ckc/make_full_h5.py
+++ ckc/make_full_h5.py
@@ -8,7 +8,7 @@ from itertools import product
 import numpy as np
 import h5py
 from multiprocessing import Pool
-#from ykc_data import full_params, param_order, hires_fstring
+from functools import partial
 
 t = [np.array([2500., 2800., 3000., 3200.]),
      np.arange(3500., 13000., 250.),
@@ -23,10 +23,23 @@ full_params = {'t': np.concatenate(t),
 
 param_order = ['t', 'g', 'feh', 'afe']
 
+# Paths and filenames
+ck_vers = 'c3k_v1.3'  # ckc_v1.2
+basedir = '/n/conroyfs1/cconroy/kurucz/grids'
+basedir = 'data/fullres'
+
+hires_dstring = os.path.join(basedir, ck_vers,
+                             "at12_feh{:+3.2f}_afe+0.0/spec/")
 hires_fstring = ("at12_feh{feh:+3.2f}_afe{afe:+2.1f}_"
                  "t{t:05.0f}g{g:.4s}.spec.gz")
-hires_dstring = "data/fullres/ckc_v1.2/at12_feh{:+3.2f}_afe+0.0/spec/"
-searchstring = 'data/fullres/dM_all/dM_feh??.??/spec/*spec.gz'
+
+lores_dstring = os.path.join(basedir, ck_vers,
+                             "at12_feh{:+3.2f}_afe+0.0/sed/")
+lores_fstring = ("at12_feh{feh:+3.2f}_afe{afe:+2.1f}_"
+                 "t{t:05.0f}g{g:.4s}.sed")
+
+searchstring = os.path.join(hires_dstring, '*spec.gz')
+#searchstring = 'data/fullres/dM_all/dM_feh??.??/spec/*spec.gz'
 
 
 def param_map(ps):
@@ -68,8 +81,8 @@ def files_and_params(searchstring=searchstring):
     return files, params
 
 
-def get_hires_spectrum(param=None, fstring=hires_fstring, dstring=hires_dstring,
-                       filename=None):
+def get_hires_spectrum(filename=None, param=None,
+                       fstring='', dstring='', **extras):
     """Read a hires spectrum from ascii files.
 
     :param param:
@@ -102,13 +115,18 @@ def get_hires_spectrum(param=None, fstring=hires_fstring, dstring=hires_dstring,
         print('did not find {}'.format(fn))
         return 0, 0, None
     fulldf = np.loadtxt(fn)
+    nc = fulldf.shape[-1]
     wave = np.array(fulldf[:,0])
     full_spec = np.array(fulldf[:,1]) # spectra
-    full_cont = np.array(fulldf[:,2]) # continuum
+    if nc > 2:
+        full_cont = np.array(fulldf[:,2]) # continuum
+    else:
+        full_cont = 0.0
+
     return full_spec, full_cont, wave
 
 
-def existing_params(params, fstring=hires_fstring, dstring=hires_dstring):
+def existing_params(params, fstring='', dstring=''):
     """Test for the existence of a spec file for each of the
     parameters in the given list, and return a list of only those
     parameters for which a spec file exists
@@ -125,7 +143,8 @@ def existing_params(params, fstring=hires_fstring, dstring=hires_dstring):
     return exists
 
 
-def specset(z, h5template='h5/ckc_feh={:+3.2f}.full.h5'):
+def specset(z, h5template='h5/ckc_feh={:+3.2f}.full.h5',
+            fstring='', dstring='', searchstring=None):
     """Make an HDF5 file containing the full resolution spectrum (and
     continuum) of all the ckc spectra with a given `feh` value.  This function
     should have minimal kwargs, so it can be easily mapped.
@@ -137,24 +156,32 @@ def specset(z, h5template='h5/ckc_feh={:+3.2f}.full.h5'):
         The oputput h5 name template
     """
     h5name = h5template.format(z)
-    paramlists = full_params.copy()
-    paramlists['feh'] = [z]
-    # 1D array of parameters
-    params = list(product(*[paramlists[p] for p in param_order]))
-    # restricted to those actually existing
-    params = existing_params(params)
+    # Get a set of existing parameters for this feh value
+    if searchstring is None:
+        # Use a specified grid of parameters
+        paramlists = full_params.copy()
+        paramlists['feh'] = [z]
+        # 1D array of parameters
+        params = list(product(*[paramlists[p] for p in param_order]))
+        # restricted to those actually existing
+        params = existing_params(params, fstring=fstring, dstring=dstring)
+    else:
+        # just pull everything from the directory matching searchstring
+        files, params = files_and_params(searchstring.format(z))
+        params = np.array(params)
+
     nspec, npar = len(params), len(params[0])
     dt = np.dtype(zip(pnames, npar * [np.float]))
     pars = np.empty(nspec, dtype=dt)
     with h5py.File(h5name, 'w') as f:
-        s, c, w = get_hires_spectrum(params[0])
+        s, c, w = get_hires_spectrum(param=params[0], fstring=fstring, dstring=dstring)
         nwave = len(w)
         spec = f.create_dataset('spectra', (nspec, nwave))
         cont = f.create_dataset('continuua', (nspec, nwave))
         pset = f.create_dataset('parameters', data=pars)
         wave = f.create_dataset('wavelengths', data=w)
         for i, p in enumerate(params):
-            s, c, w = get_hires_spectrum(p)
+            s, c, w = get_hires_spectrum(param=p, fstring=fstring, dstring=dstring)
             pset[i] = tuple(param_map(list(p)))
             if w is None:
                 continue
@@ -168,38 +195,6 @@ def specset(z, h5template='h5/ckc_feh={:+3.2f}.full.h5'):
     return h5name
 
 
-def specall(searchstring, h5name='test.h5'):
-    """Make an HDF5 file from all spectra that match the pattern given by
-    searchstring.  Parameters from each file are automatically extracted from
-    the filename.
-    """
-    files, params = files_and_params(searchstring)
-    nspec, npar = len(params), len(params[0])
-    assert npar == len(pnames)
-    dt = np.dtype(zip(pnames, npar * [np.float]))
-    pars = np.empty(nspec, dtype=dt)
-    with h5py.File(h5name, 'w') as f:
-        s, c, w = get_hires_spectrum(filename=files[0])
-        nwave = len(w)
-        spec = f.create_dataset('spectra', (nspec, nwave))
-        cont = f.create_dataset('continuua', (nspec, nwave))
-        pset = f.create_dataset('parameters', data=pars)
-        wave = f.create_dataset('wavelengths', data=w)
-        for i, (fn, p) in enumerate(zip(files, params)):
-            s, c, w = get_hires_spectrum(filename=fn)
-            pset[i] = tuple(param_map(list(p)))
-            if w is None:
-                continue
-            try:
-                spec[i,:] = s
-                cont[i,:] = c
-            except:
-                spec[i,:] = 0
-                cont[i,:] = 0
-                print('problem storing spectrum @ params {}'.format(dict(zip(param_order, p))))
-        
-
-
 if __name__ == "__main__":
 
     ncpu = 6
@@ -211,9 +206,11 @@ if __name__ == "__main__":
              -1.75, -1.25, -0.75, -0.25, 0.25, 0.75]
     #zlist = full_params['feh']
     #zlist = [0.5]
-    
+
+    partial_specset = partial(specset, h5template='h5/ckc_feh={:+3.2f}.full.h5',
+                              dstring=hires_dstring, fstring=hires_fstring)
     ts = time.time()
-    filenames = list(M(specset, list(zlist)))
+    filenames = list(M(partial_specset, list(zlist)))
     dur = time.time() - ts
 
     print(filenames)
