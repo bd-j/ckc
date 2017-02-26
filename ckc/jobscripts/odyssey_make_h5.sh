98b4830c8d4ce50ca78ec834e51891c3ffdd4603
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2017-02-26 15:34:48 -0500

    jobscript for odyssey

---
+++ ckc/jobscripts/odyssey_make_h5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+#SBATCH -J c3k_to_h5
+#SBATCH -n 20 # Number of cores requested
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -t 6:00:00 # Runtime 
+#SBATCH -p conroy # Partition to submit to
+#SBATCH --mem-per-cpu=2000 # Memory per node in MB (see also --mem-per-cpu)
+#SBATCH -o /n/regal/conroy_lab/bdjohnson/ckc/ckc/h5/logs/c3k_to_h5_%A_%a.out # Standard out goes to this file
+#SBATCH -e /n/regal/conroy_lab/bdjohnson/ckc/ckc/h5/logs/c3k_to_h5_%A_%a.err # Standard err goes to this file
+
+ncpu=$SLURM_JOB_CPUS_PER_NODE
+
+source activate pympi
+cd /n/regal/conroy_lab/bdjohnson/ckc/ckc/
+python make_full_h5.py $ncpu
