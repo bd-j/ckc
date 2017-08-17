ba95a7fd9a968b67bf7ac230f23de0c5f5a95a67
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2017-08-17 16:10:35 -0400

    more qa plotting

--- ckc/qa/show_coverage.py
+++ ckc/qa/show_coverage.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('AGG')
 import matplotlib.pyplot as pl
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
@@ -5,9 +7,9 @@ import glob
 import h5py
 import re
 
-def plot_hrd(params):
+def plot_hrd(params, **plot_kwargs):
     fig, ax = pl.subplots()
-    ax.plot(params['logt'], params['logg'], 'o')
+    ax.plot(params['logt'], params['logg'], 'o', **plot_kwargs)
     ax.set_xlim(4.7, 3.3)
     ax.set_ylim(5.6, -1.1)
     ax.set_ylabel('log g')
@@ -26,22 +28,43 @@ def get_abundance(files):
         afe = afe = len(feh) * [0.0]
     
     return feh, afe
-        
-strfmt = '$N_{{spec}}={},\, N_{{pix}}={}$\n$[Fe/H]={}, \, [\\alpha/Fe]={}$'
 
-pdf = PdfPages('c3k_hrd.pdf')
-#files = np.sort(glob.glob("h5/*h5"))
-files = ['../fullres/c3k/c3k_v1.3_feh+0.00.full.h5']
-feh, afe = get_abundance(files)
 
-for i, fn in enumerate(files):
-    with h5py.File(fn, "r") as dat:
-        nw = dat['wavelengths'].shape
-        params = dat['parameters'][:]
-    fig, ax = plot_hrd(params)
-    ax.text(0.1, 0.85, strfmt.format(len(params), nw[0], feh[i], afe[i]),
-            transform=ax.transAxes)
-    pdf.savefig(fig)
-    pl.close(fig)
+def plot_isoc(isoc, logage=9.0, feh=0.0,
+              ax=None, **plot_kwargs):
+    sel = (isoc['EAF']['log_age'] == logage) & (isoc['EAF']['initial_[Fe/H]'] == feh)
+    x = isoc['BSP'][sel]['log_Teff']
+    y = isoc['BSP'][sel]['log_g']
+    order = np.argsort(isoc['EAF'][sel]['EEP'])
+    ax.plot(x[order], y[order], **plot_kwargs)
+    return ax
 
-pdf.close()
+
+if __name__ == "__main__":
+
+    isocfile = 'MIST_full.h5'
+    isoc = h5py.File(isocfile, "r")
+    strfmt = '$N_{{spec}}={},\, N_{{pix}}={}$\n$[Fe/H]={}, \, [\\alpha/Fe]={}$'
+
+    ages = [7, 8, 9, 9.5, 10]
+    isocolor = [u'#5DA5DA', u'#FAA43A', u'#60BD68', u'#F15854', u'#39CCCC',
+                u'#FF70B1', u'#FFDC00', u'#85144B', u'#4D4D4D']
+    pdf = PdfPages('c3k_hrd.pdf')
+
+    files = ['../fullres/c3k/c3k_v1.3_feh+0.00.full.h5']
+    files = np.sort(glob.glob("/n/home02/bdjohnson/fs/data/stars/c3k_v1.3/h5/*h5"))
+    feh, afe = get_abundance(files)
+
+    for i, fn in enumerate(files):
+        with h5py.File(fn, "r") as dat:
+            nw = dat['wavelengths'].shape
+            params = dat['parameters'][:]
+        fig, ax = plot_hrd(params, color='k')
+        ax.text(0.1, 0.85, strfmt.format(len(params), nw[0], feh[i], afe[i]),
+                transform=ax.transAxes)
+        for j, age in enumerate(ages):
+            plot_isoc(isoc, logage=age, feh=feh[i], ax=ax, color=isocolor[j])
+        pdf.savefig(fig)
+        pl.close(fig)
+
+    pdf.close()
